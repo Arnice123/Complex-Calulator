e5c85a35c749167c8e87f7dce3b875421bfa0b5c
Arya Akhavein <aryaakhavein92@gmail.com>
2024-12-12 14:33:21 -0800

    Added the base files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <complex_opps.h>
+#include <basic_opps.h>
+
+using namespace std;
+
+int main()
+{
+    int operation;
+    cout << "Enter an operation [1]-Add [2]-Subtract [3]-Division [4]-Multiplication: ";
+    cin >> operation;
+    cout << endl;
+    return;
+}
